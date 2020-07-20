8b1fd3cf594a02fa8db18f788cdc634678c4e301
rogerteles13 <rogerteles13@gmail.com>
2020-07-19 23:32:28 -0300

    inicio do modulo express no curso de node

--- site ola mundo/js/app.js
+++ site ola mundo/js/app.js
@@ -1,1 +1,15 @@
-console.log ("hello word!")
+//console.log ("hello word!")
+
+const express = require("express")
+const app = express()
+
+//contruindo a rota para o servidor express
+
+app.get("/", function(req, res){
+    res.send("Seja bem vindo ao servidor construido com o Express")
+})
+
+//servidor atravé do express//sempre no final do código
+app.listen(8081, function(){
+    console.log ("hello word! servidor rodando")
+    }) 
